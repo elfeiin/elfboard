9a876b4b2224cc557130a77a56f7fe4c799564fc
Elfeiin <patlymlatun@gmail.com>
2023-04-28 00:57:26 -0700

    Begin conversion to android/mobile app

---
+++ src-tauri/src/lib.rs
@@ -0,0 +1,141 @@
+use std::{
+    ops::{Mul, Not, Shl},
+    sync::Mutex,
+};
+
+#[cfg(mobile)]
+mod mobile;
+#[cfg(mobile)]
+pub use mobile::*;
+use tauri::App;
+
+// const COMMANDS: &[u8] = &[];
+
+pub type SetupHook = Box<dyn FnOnce(&mut App) -> Result<(), Box<dyn std::error::Error>> + Send>;
+
+// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+
+#[derive(Default)]
+pub struct AppBuilder {
+    setup: Option<SetupHook>,
+}
+
+impl AppBuilder {
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    #[must_use]
+    pub fn setup<F>(mut self, setup: F) -> Self
+    where
+        F: FnOnce(&mut App) -> Result<(), Box<dyn std::error::Error>> + Send + 'static,
+    {
+        self.setup.replace(Box::new(setup));
+        self
+    }
+
+    pub fn run(self) {
+        let setup = self.setup;
+        tauri::Builder::default()
+            .setup(move |app| {
+                if let Some(setup) = setup {
+                    (setup)(app)?;
+                }
+                Ok(())
+            })
+            .run(tauri::generate_context!())
+            .expect("error while running tauri application");
+    }
+}
+
+#[derive(Default)]
+struct Utf8Codepoint {
+    char_buf: Vec<u8>,
+}
+
+impl Utf8Codepoint {
+    pub fn is_valid(&self) -> bool {
+        match self.char_buf.as_slice() {
+            [0..=127] => true,                                    // 1 byte
+            [192..=223, 128..=191] => true,                       // 2 byte
+            [224..=239, 128..=191, 128..=191] => true,            // 3 byte
+            [240..=247, 128..=191, 128..=191, 128..=191] => true, // 4 byte
+            _ => false,
+        }
+    }
+
+    pub fn to_str(&self) -> String {
+        unsafe { String::from_utf8_unchecked(self.char_buf.clone()) }
+    }
+
+    // pub fn to_u32(&self) -> u32 {
+    //     let mut a = [0; 4];
+    //     for (i, b) in self.char_buf.iter().enumerate() {
+    //         a[i] = *b;
+    //     }
+    //     u32::from_be_bytes(a)
+    // }
+
+    pub fn current_byte(&mut self) -> Option<&mut u8> {
+        self.char_buf.last_mut()
+    }
+
+    pub fn push(&mut self, b: u8) {
+        self.char_buf.push(b);
+    }
+
+    pub fn reset(&mut self) {
+        *self = Self::default()
+    }
+
+    pub fn update_current_byte(&mut self, column: u8, row: u8) {
+        let b = if let Some(b) = self.current_byte() {
+            b
+        } else {
+            self.char_buf.push(row.shl(column.mul(2)));
+            return;
+        };
+        *b &= 3u8.shl(column.mul(2)).not();
+        *b |= row.shl(column.mul(2));
+    }
+}
+
+#[derive(Default)]
+struct KeyboardState {
+    codepoint: Utf8Codepoint,
+    pressed: Option<String>,
+}
+
+#[derive(Default)]
+struct AppState {
+    kb_state: Mutex<KeyboardState>,
+}
+
+#[tauri::command]
+fn entered_key(column: u8, row: u8, state: tauri::State<AppState>) {
+    assert![column < 4];
+    assert![row < 4];
+    let mut kb_state = state.kb_state.lock().unwrap();
+    kb_state.pressed = None;
+    kb_state.codepoint.update_current_byte(column, row);
+}
+
+#[tauri::command]
+fn entered_end(state: tauri::State<AppState>) {
+    let mut kb_state = state.kb_state.lock().unwrap();
+    if kb_state.codepoint.is_valid() {
+        kb_state.pressed = Some(kb_state.codepoint.to_str());
+        kb_state.codepoint.reset();
+    } else if kb_state.codepoint.char_buf.len() < 4 {
+        kb_state.codepoint.push(0);
+    } else {
+        kb_state.codepoint.reset();
+    }
+}
+
+#[tauri::command]
+fn lifted(state: tauri::State<AppState>) {
+    let mut kb_state = state.kb_state.lock().unwrap();
+    kb_state.pressed = None;
+    kb_state.codepoint.reset();
+}
--- ui/main.js
+++ ui/main.js
@@ -1,33 +1,29 @@
 const { invoke } = window.__TAURI__.tauri;
 
-async function set(i, n) {
-  await invoke("set", { index: i, value: n });
+async function entered_key(n) {
+  await invoke("entered_key", { column: Math.floor(n / 4), row: n % 4 });
 }
 
-async function exited(i) {
-  await invoke("exited", { index: i });
+async function entered_end() {
+  await invoke("entered_end", {});
 }
 
-async function reset() {
-  await invoke("reset", {});
+async function lifted() {
+  await invoke("lifted", {});
 }
 
 window.addEventListener("DOMContentLoaded", () => {
   document
     .querySelector("body")
-    .addEventListener("touchend", () => reset());
-  document
-    .querySelector("#leftinator")
-    .addEventListener("mouseenter", () => exited(0));
-  document
-    .querySelector("#rightinator")
-    .addEventListener("mouseenter", () => exited(3));
-  for (let i = 0; i < 4; i++) {
-    for (let n = 0; n < 4; n++) {
-      let q = ("#q" + i) + n;
-      document
-        .querySelector(q)
-        .addEventListener("mouseover", () => set(i, n));
-    }
+    .addEventListener("touchend", () => lifted());
+  for (let i = 0; i < 16; i++) {
+    let q = "#k" + i;
+    document
+      .querySelector(q)
+      .addEventListener("mouseover", () => entered_key(i));
   }
+  document.querySelector("#left")
+    .addEventListener("mouseover", () => entered_end());
+  document.querySelector("#right")
+    .addEventListener("mouseover", () => entered_end());
 });
