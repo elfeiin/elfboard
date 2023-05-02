f67dd4a15e636d1ebe05a65197f35a2699150c35
Elfeiin <patlymlatun@gmail.com>
2023-05-01 20:12:10 -0700

    touching! :D

--- ui/main.js
+++ ui/main.js
@@ -10,20 +10,35 @@ async function entered_end() {
 
 async function lifted() {
   await invoke("lifted", {});
+  document.querySelectorAll(".shine").forEach((e) => {
+    e.classList.remove("shine");
+  });
+}
+
+async function swipe_hover(e) {
+  let cx = e.touches[0].clientX;
+  let cy = e.touches[0].clientY;
+  let e = document.elementFromPoint(cx,cy);
+  document.querySelectorAll(".shine").forEach((e) => {
+    e.classList.remove("shine");
+  });
+  if (e.classList.contains("key")) {
+    e.classList.add("shine");
+  }
+  // if (e.key_num != null) {
+  //   entered_key(e.key_num);
+  // } else if (e.id == "left" || e.id == "right") {
+  //   entered_end();
+  // }
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  document
-    .querySelector("body")
-    .addEventListener("touchend", () => lifted());
+  let body = document
+    .querySelector("body");
+  body.addEventListener("touchmove", swipe_hover);
+  body.addEventListener("touchend", () => lifted());
   for (let i = 0; i < 16; i++) {
     let q = "#k" + i;
-    document
-      .querySelector(q)
-      .addEventListener("touchmove", () => entered_key(i));
+    // document.querySelector(q).key_num = i;
   }
-  document.querySelector("#left")
-    .addEventListener("touchmove", () => entered_end());
-  document.querySelector("#right")
-    .addEventListener("touchmove", () => entered_end());
 });
